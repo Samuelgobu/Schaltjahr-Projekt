c1a780de2edb36dbeed012bb8bd95d66c6963e8c
Joshua Roshen <joshua.roshen.smith@gmail.com>
2022-03-23 08:24:48 +0100

    Projektdateien hinzufügen.

---
+++ Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+//Der Benutzer soll nach einer jahreszahle gefragt werden und es
+//soll ausgegeben werden ob es sich um ein Shaltjahr handelt
+
+namespace Schaltjahr_Projekt
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int h;
+
+
+            Console.WriteLine("Bitte schreiben Sie ein jahr und das Programm wird zeigen, ob es ein Schaltjahr ist oder nein");
+            Console.WriteLine("Jahr=");
+            h = Convert.ToInt32(Console.ReadLine());
+
+            if (h % 4 == 0)
+            {
+                if (h % 100 == 0)
+                {
+                    if (h % 400 == 0)
+                    {
+                        Console.WriteLine("Das ist ein Schaltjahr");
+                    }
+                    else
+                    {
+                        Console.WriteLine("Das ist kein Schaltjahr");
+                    }
+                }
+                else
+                {
+                    Console.WriteLine("Das ist ein Schaltjahr");
+
+                }
+
+            }
+            else
+            {
+                Console.WriteLine("Das ist kein Schaltjahr");
+            }
+        }
+    }
+}
